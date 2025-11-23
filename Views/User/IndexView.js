c5b467b92c99651ae0bf4b3e093b6792231c54af
Fhonq-22 <hgfog8310a1@gmail.com>
2025-11-23 16:53:31 +0700

    Tạo view cho trang chủ người dùng

---
+++ Views/User/IndexView.js
@@ -0,0 +1,43 @@
+import { layDanhSachBoDe, layBoDe } from "../Controllers/BoDeController.js";
+
+$(document).ready(async function() {
+    const username = localStorage.getItem("currentUser") || "Khách";
+    $("#userDisplay").text(username);
+
+    $("#btnLogout").click(() => {
+        localStorage.removeItem("currentUser");
+        window.location.href = "dang-nhap.html";
+    });
+
+    await loadBoDe();
+});
+
+async function loadBoDe() {
+    const danhSach = await layDanhSachBoDe();
+    const tbody = $("#boDeTable tbody");
+    tbody.empty();
+
+    for (let ma of danhSach) {
+        const bd = await layBoDe(ma);
+        const row = $(`
+            <tr>
+                <td>${ma}</td>
+                <td>${bd.TenBoDe}</td>
+                <td>${bd.SoCauHoi}</td>
+                <td>${bd.ThoiGian}</td>
+                <td>${bd.NgayTao}</td>
+                <td>${bd.NguoiTao}</td>
+                <td>
+                    <button class="startBtn">Bắt đầu</button>
+                </td>
+            </tr>
+        `);
+
+        row.find(".startBtn").click(() => {
+            localStorage.setItem("currentBoDe", ma);
+            window.location.href = "lam-bai.html";
+        });
+
+        tbody.append(row);
+    }
+}
