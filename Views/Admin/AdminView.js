70c411c5af6741225e06bb163b48acee6ccba7e5
Phòng <113459855+Fhonq-22@users.noreply.github.com>
2026-01-29 14:17:25 +0700

    Cập nhật UX trang quản trị

--- Views/Admin/AdminView.js
+++ Views/Admin/AdminView.js
@@ -3,8 +3,8 @@ import { yeuCauAdmin, removeAuth } from "../../Utils/AUTH.js";
 $(document).ready(function () {
     if (!yeuCauAdmin()) return;
 
-    $("#btnLogout").click(() => {
+    $("#btnLogout").on("click", function () {
         removeAuth();
-        window.location.href = "dang-nhap.html";
+        window.location.replace("dang-nhap.html");
     });
 });
